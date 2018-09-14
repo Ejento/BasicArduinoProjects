bde0bcc968f0d2f5793eacee5b17c723cab2146f
George <exploitme@protonmail.com>
2018-09-14 11:51:45 +0300

    Added Images

--- Four_Leds_Blink/Four_Leds_Blink.ino
+++ Four_Leds_Blink/Four_Leds_Blink.ino
@@ -1,9 +1,26 @@
+// First we create a list of all the pins that we gonna use.
+// Each digital pin from our Arduino is connected to an LED, so we can name this array as leds.
+// I was using 3 different standalone leds and one RGB led for this project. My setup was something like this:
+// Arduino pin 5 = Red LED
+// Arduino pin 6 = Green LED
+// Arduino pin 7 = Blue LED
+// Arduino pin 8 = The blue color from a RGB LED
+// Arduino pin 9 = The red color from a RGB LED
+// Arduino pin 10 = The green color from a RGB LED
 int leds[] = { 5, 6, 7, 8, 9, 10 };
+
+// Because we want to have some delay between each action, we are initiating a timer here. The timer is in miliseconds, so 1000 ms = 1 sec.
 int timer = 1000;
-int lastLed = 0;
-int randomLed = 0;
+
+// We want to add some randomness in our project, so we are using the next to variables to add that.
+int lastLed = 0; // Saves the last LED that has been turned on and off.
+int randomLed = 0; // Saves the current random LED 
+
 void setup() {
-  Serial.begin(9600);
+ 
+ // Here we are setting up our hardware. We want all the pins to act as outputs, so we are using the command pinMode to do that.
+  // That could be enough, but I wanted to do a test drive to make sure that the LEDs are working as they should be.
+  // In the first loop I turn on each LED on and off, and with the last two loops, I turn all the LEDs on and off at the same time.
   for (int i = 0; i < (sizeof(leds) / sizeof(int)); i++) {
     pinMode(leds[i], OUTPUT);
     digitalWrite(leds[i], HIGH);
@@ -12,25 +29,36 @@ void setup() {
     delay(timer);
   }
   delay(timer);
+  
+  // All the LEDs turned on
   for (int i = 0; i < (sizeof(leds) / sizeof(int)); i++) {
     digitalWrite(leds[i], HIGH);
   }
+  // 1 secend delay
   delay(timer);
+  
+  // All the LEDs turned off
   for (int i = 0; i < (sizeof(leds) / sizeof(int)); i++) {
     digitalWrite(leds[i], LOW);
   }
 }
 
+// Here is my main function.
 void loop() {
+  // I choose a number between 0 and 5. I choose 6 because if you look at the array leds, you can see that I am using 6 LEDs for this project.
   randomLed = random(6);
-  Serial.print("The random number is ");
-  Serial.println(randomLed);
+  
+  // If the random choosen LED is the same as the last LED, then we skip that and we start over.
   if (randomLed != lastLed) {
-    if (randomLed >= 3) {
+	  
+	// If the random LED is one of the standalone LEDs, then we turned it on and off with a delay.
+    if (randomLed <= 2) {
       digitalWrite(leds[randomLed], HIGH);
       delay(timer);
       digitalWrite(leds[randomLed], LOW);
       delay(timer);
+	  
+	// If the random LED is the RGB LED, then I am randomizing again the effect to get two colors to turn on at the same time. Still there is a chance to see only one color.
     } else {
       int randomRGB = random(3);
       digitalWrite(leds[randomLed], HIGH);
@@ -41,5 +69,7 @@ void loop() {
       delay(timer);
     }
   }
+  
+  // I save the last led that was been used.
   lastLed = randomLed;
 }
--- Four_Leds_Fade/Four_Leds_Fade.ino
+++ Four_Leds_Fade/Four_Leds_Fade.ino
@@ -1,72 +1,90 @@
-int pins[] = { 11, 10, 9 , 6, 5, 3 };
+// First we create a list of all the pins that we gonna use.
+// Each digital pin from our Arduino is connected to an LED, so we can name this array as leds.
+// I was using 3 different standalone leds and one RGB led for this project. My setup was something like this:
+// Arduino pin 3 = Red LED
+// Arduino pin 5 = Green LED
+// Arduino pin 6 = Blue LED
+// Arduino pin 9 = The blue color from the RGB LED
+// Arduino pin 10 = The red color from the RGB LED
+// Arduino pin 11 = The green color from the RGB LED
+int leds[] = { 11, 10, 9 , 6, 5, 3 };
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  for (int i = 0; i < (sizeof(pins) / sizeof(int)); i++) {
-    pinMode(pins[i], OUTPUT);
+  // I am setting up the hardware for this project, as we want each pin to act as output.
+  for (int i = 0; i < (sizeof(leds) / sizeof(int)); i++) {
+    pinMode(leds[i], OUTPUT);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("1st loop!");
+  // This loop turns on the LED little by little with a very small, unnoticed delay.
   for (int shade = 0; shade <= 255; shade += 1) {
-    analogWrite(pins[2], shade);
+    analogWrite(leds[2], shade);
     delay(15);
   }
-  Serial.println("2nd loop!");
+  // This loop turns on the LED little by little with a very small, unnoticed delay.
   for (int shade = 0; shade <= 255; shade += 1) {
-    analogWrite(pins[1], shade);
+    analogWrite(leds[1], shade);
     delay(15);
   }
-  Serial.println("3rd loop!");
+
+  // This loop turns on the LED little by little with a very small, unnoticed delay.
   for (int shade = 0; shade <= 255; shade += 1) {
-    analogWrite(pins[0], shade);
+    analogWrite(leds[0], shade);
     delay(15);
   }
-  Serial.println("4th loop!");
+  
+  // This loop turns off the LED little by little with a very small, unnoticed delay. 
   for (int shade = 255; shade >= 0; shade -= 1) {
-    analogWrite(pins[1], shade);
+    analogWrite(leds[1], shade);
     delay(15);
   }
-  Serial.println("5th loop!");
+
+  // This loop turns off the LED little by little with a very small, unnoticed delay.
   for (int shade = 255; shade >= 0; shade -= 1) {
-    analogWrite(pins[0], shade);
+    analogWrite(leds[0], shade);
     delay(15);
   }
-  Serial.println("6th loop!");
+
+  // This loop turns off the LED little by little with a very small, unnoticed delay.
   for (int shade = 255; shade >= 0; shade -= 1) {
-    analogWrite(pins[2], shade);
+    analogWrite(leds[2], shade);
     delay(15);
   }
-  Serial.println("7th loop!");
+
+  // This loop turns on the LED little by little with a very small, unnoticed delay.
   for (int shade = 0; shade <= 255; shade += 5) {
-    analogWrite(pins[3], shade);
+    analogWrite(leds[3], shade);
     delay(60);
   }
-  Serial.println("8th loop!");
+
+  // This loop turns off the LED little by little with a very small, unnoticed delay.
   for (int shade = 255; shade >= 0; shade -= 5) {
-    analogWrite(pins[3], shade);
+    analogWrite(leds[3], shade);
     delay(60);
   }
-  Serial.println("9th loop!");
+
+  // This loop turns on the LED little by little with a very small, unnoticed delay.
   for (int shade = 0; shade <= 255; shade += 5) {
-    analogWrite(pins[4], shade);
+    analogWrite(leds[4], shade);
     delay(60);
   }
-  Serial.println("10th loop!");
+
+  // This loop turns off the LED little by little with a very small, unnoticed delay.
   for (int shade = 255; shade >= 0; shade -= 5) {
-    analogWrite(pins[4], shade);
+    analogWrite(leds[4], shade);
     delay(60);
   }
-  Serial.println("11th loop!");
+
+  // This loop turns on the LED little by little with a very small, unnoticed delay.
   for (int shade = 0; shade <= 255; shade += 5) {
-    analogWrite(pins[5], shade);
+    analogWrite(leds[5], shade);
     delay(60);
   }
-  Serial.println("12th loop!");
+
+  // This loop turns off the LED little by little with a very small, unnoticed delay.
   for (int shade = 255; shade >= 0; shade -= 5) {
-    analogWrite(pins[5], shade);
+    analogWrite(leds[5], shade);
     delay(60);
   }
 }
